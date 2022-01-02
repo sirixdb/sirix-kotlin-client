9d41d7ce8c977fa761f80fb0d19647d25082511a
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2022-01-01 21:34:58 -0500

    add more resource functions

--- src/main/kotlin/Resource.kt
+++ src/main/kotlin/Resource.kt
@@ -1,6 +1,7 @@
 import wrappers.AbstractClientWrapper
 import wrappers.Response
 import wrappers.SirixClientUsageException
+import java.io.Serializable
 
 class Resource(
     val databaseName: String,
@@ -17,16 +18,83 @@ class Resource(
         ).statusCode != 404
     }
 
+    suspend fun create(data: String, hashType: HashType): Response {
+        return checkResponse(
+            httpClient.put(
+                path, mapOf("hashType" to hashType.toString()), mapOf("content-type" to httpType), data
+            )
+        )
+    }
+
     suspend fun history(): List<Commit> {
         return parseToJson<History>(
             checkResponse(
                 httpClient.get(
-                    "$databaseName/$resourceName/history",
-                    mapOf(),
-                    mapOf("accept" to httpType)
+                    "$databaseName/$resourceName/history", mapOf(), mapOf("accept" to httpType)
                 )
             )
         ).history
+    }
+
+    suspend fun read(
+        nodeId: ULong? = null,
+        maxLevel: UInt? = null,
+        topLevelLimit: UInt? = null,
+        topLevelSkipLastNode: UInt? = null,
+        revision: Revision? = null,
+        firstRevision: Revision? = null,
+        lastRevision: Revision? = null
+    ): Response {
+        val params = buildReadParams(
+            nodeId, maxLevel, topLevelLimit, topLevelSkipLastNode, revision, firstRevision, lastRevision, null
+        )
+        return checkResponse(httpClient.get(path, params, mapOf("accept" to httpType)))
+    }
+
+    suspend inline fun <reified T> readAndParse(
+        nodeId: ULong? = null,
+        maxLevel: UInt? = null,
+        topLevelLimit: UInt? = null,
+        topLevelSkipLastNode: UInt? = null,
+        revision: Revision? = null,
+        firstRevision: Revision? = null,
+        lastRevision: Revision? = null
+    ): T {
+        return parseToJson(
+            read(
+                nodeId, maxLevel, topLevelLimit, topLevelSkipLastNode, revision, firstRevision, lastRevision
+            )
+        )
+    }
+
+    suspend fun readWithMetadata(
+        nodeId: ULong? = null,
+        maxLevel: UInt? = null,
+        topLevelLimit: UInt? = null,
+        topLevelSkipLastNode: UInt? = null,
+        revision: Revision? = null,
+        firstRevision: Revision? = null,
+        lastRevision: Revision? = null,
+        metadataType: MetadataType = MetadataType.ALL
+    ): Response {
+        /// TODO: change return type to metadata response type, parse json response
+        val params = buildReadParams(
+            nodeId, maxLevel, topLevelLimit, topLevelSkipLastNode, revision, firstRevision, lastRevision, metadataType
+        )
+        return checkResponse(httpClient.get(path, params, mapOf("accept" to httpType)))
+    }
+
+    suspend fun diff(firstRevision: Revision, secondRevision: Revision, nodeId: ULong?, maxDepth: UInt?): DiffResponse {
+        val params = mutableMapOf<String, Serializable>()
+        params["first-revision"] = firstRevision.toString()
+        params["second-revision"] = secondRevision.toString()
+        if (nodeId != null) {
+            params["startNodeKey"] = nodeId.toString()
+        }
+        if (maxDepth != null) {
+            params["maxDepth"] = maxDepth.toString()
+        }
+        return parseToJson(checkResponse(httpClient.get("$databaseName/$resourceName/diff", params, mapOf())))
     }
 
     suspend fun etag(nodeId: ULong): String {
@@ -50,6 +118,18 @@ class Resource(
         return checkResponse(httpClient.get(path, params, mapOf("accept" to httpType)))
     }
 
+    suspend fun update(nodeId: ULong, data: String, insert: Insert = Insert.CHILD, etag: String?): Response {
+        val etagToUse = etag ?: etag(nodeId)
+        return checkResponse(
+            httpClient.post(
+                path,
+                mapOf("nodeId" to nodeId.toString(), "insert" to insert),
+                mapOf("etag" to etagToUse, "content-type" to httpType),
+                data
+            )
+        )
+    }
+
     suspend fun delete(nodeId: ULong?, etag: String?): Response {
         var etagAssignable = etag
         if ((nodeId == null) && (etag != null)) {
@@ -68,4 +148,48 @@ class Resource(
         }
         return checkResponse(httpClient.delete(path, params, headers))
     }
+}
+
+private fun buildReadParams(
+    nodeId: ULong?,
+    maxLevel: UInt?,
+    topLevelLimit: UInt?,
+    topLevelSkipLastNode: UInt?,
+    revision: Revision?,
+    firstRevision: Revision?,
+    lastRevision: Revision?,
+    metadataType: MetadataType?,
+): Map<String, Serializable> {
+    val params = mutableMapOf<String, Serializable>()
+    if (nodeId != null) {
+        params["nodeId"] = nodeId.toString()
+    }
+    if (maxLevel != null) {
+        params["maxLevel"] = maxLevel.toString()
+    }
+    if (topLevelLimit != null) {
+        params["nextTopLevelNodes"] = topLevelLimit.toString()
+    }
+    if (topLevelSkipLastNode != null) {
+        params["lastTopLevelNodeKey"] = topLevelSkipLastNode.toString()
+    }
+    if (revision != null) {
+        when (revision) {
+            is Revision.RevisionNumber -> params["revision"] = revision.number.toString()
+            is Revision.RevisionTimestamp -> params["revision-timestamp"] = revision.timestamp
+        }
+    } else if (firstRevision != null && lastRevision != null) {
+        when (firstRevision) {
+            is Revision.RevisionNumber -> params["start-revision"] = firstRevision.number.toString()
+            is Revision.RevisionTimestamp -> params["start-revision-timestamp"] = firstRevision.timestamp
+        }
+        when (lastRevision) {
+            is Revision.RevisionNumber -> params["start-revision"] = lastRevision.number.toString()
+            is Revision.RevisionTimestamp -> params["start-revision-timestamp"] = lastRevision.timestamp
+        }
+    }
+    if (metadataType != null) {
+        params["withMetadata"] = metadataType
+    }
+    return params
 }
--- src/main/kotlin/types.kt
+++ src/main/kotlin/types.kt
@@ -1,13 +1,48 @@
 import kotlinx.serialization.Serializable
+import java.time.LocalDate
 
 enum class DatabaseType {
-    JSON,
-    XML;
+    JSON, XML;
 
     fun toHttpType() = when (this) {
         JSON -> "application/json"
         XML -> "application/xml"
     }
+}
+
+enum class HashType {
+    ROLLING, NONE;
+
+    override fun toString() = when (this) {
+        ROLLING -> "ROLLING"
+        NONE -> "NONE"
+    }
+}
+
+enum class MetadataType {
+    ALL, KEY, NODE_KEY_AND_CHILD_COUNT;
+
+    override fun toString(): String = when (this) {
+        ALL -> "true"
+        KEY -> "nodeKey"
+        NODE_KEY_AND_CHILD_COUNT -> "nodeKeyAndChildCount"
+    }
+}
+
+enum class Insert {
+    CHILD, LEFT, RIGHT, REPLACE;
+
+    override fun toString(): String = when (this) {
+        CHILD -> "asFirstChild"
+        LEFT -> "asLeftSibling"
+        RIGHT -> "asRightSibling"
+        REPLACE -> "replace"
+    }
+}
+
+open class Revision private constructor() {
+    class RevisionNumber(val number: ULong?) : Revision()
+    class RevisionTimestamp(val timestamp: LocalDate) : Revision()
 }
 
 @Serializable
@@ -33,4 +68,15 @@ data class Commit(
     val revision: ULong,
     val author: String,
     val commitMessage: String,
+)
+
+@Serializable
+data class DiffResponse(
+    val diffs: List<Diff>
+)
+
+@Serializable
+data class Diff(
+    /// TODO fix this
+    val dummyValue: String
 )
