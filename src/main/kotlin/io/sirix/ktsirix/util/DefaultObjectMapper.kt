d96cb57c75e8d852275fc16a7dff7867cc5ee1a9
Arturo <arturoarl294@gmail.com>
2022-10-20 00:30:57 +0200

    chore: change joda time for java time serialization and set include non null fields for serialization

--- src/main/kotlin/io/sirix/ktsirix/util/DefaultObjectMapper.kt
+++ src/main/kotlin/io/sirix/ktsirix/util/DefaultObjectMapper.kt
@@ -1,16 +1,18 @@
 package io.sirix.ktsirix.util
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.datatype.joda.JodaModule
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
 object DefaultObjectMapper : ObjectMapper() {
     init {
         propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
         disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-        registerModule(JodaModule())
+        registerModule(JavaTimeModule())
         registerKotlinModule()
+        setSerializationInclusion(JsonInclude.Include.NON_NULL)
     }
 }
