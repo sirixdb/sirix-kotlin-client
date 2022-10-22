704f515ec3c7f3d9375cc9087522d0d7123bf811
Arturo <arturoarl294@gmail.com>
2022-10-23 01:31:45 +0200

    chore: use the client interface in the Sirix class

--- src/main/kotlin/io/sirix/ktsirix/Sirix.kt
+++ src/main/kotlin/io/sirix/ktsirix/Sirix.kt
@@ -2,19 +2,10 @@ package io.sirix.ktsirix
 
 import com.fasterxml.jackson.core.type.TypeReference
 
-class Sirix {
-    private val client: SyncClient
+class Sirix(
+    private val client: ApiClient,
     private val authManager: AuthenticationManager
-
-    constructor(basePath: String, username: String, password: String) {
-        client = SyncClient(basePath)
-        authManager = AuthenticationManager(username, password, client)
-    }
-
-    internal constructor(client: SyncClient, authManager: AuthenticationManager) {
-        this.client = client
-        this.authManager = authManager
-    }
+) {
 
     fun database(name: String, type: DbType): Database = Database(name, type, client, authManager)
 
